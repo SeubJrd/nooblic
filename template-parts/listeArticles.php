fc46261d8406c349fe1172ebce9889de40dae171
Sébastien Jourdain <sebj411@gmail.com>
2021-05-17 15:16:05 +0200

    Ajout des fichiers de Clément
    voir titre

---
+++ template-parts/listeArticles.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: listeArticles
+
+ClÃ©ment
+*/
+?>
+
+<?php
+get_header();
+?>
+
+    <main id="event" class="header-correction">
+
+        <section class="titre page">
+            <h1 class="entry-title">Liste des Articles</h1>
+        </section>
+
+
+            <?php the_content() ?>
+
+
+            <div class="liste">
+
+                <div class="grille">
+                    <ul>
+                        <?php get_template_part('single', 'liste'); ?>
+                    </ul>
+                </div>
+            </div>
+
+
+    </main>
+
+
+<?php
+get_footer();
+?>
