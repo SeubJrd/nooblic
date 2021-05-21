76e9b02c69fdfc8cea79c6abf8bae7e96cce1c62
Villa <clement.villa@edu.univ-fcomte.fr>
2021-05-21 12:12:11 +0200

    remise en place de liste articles

--- single-liste.php
+++ single-liste.php
@@ -13,44 +13,43 @@ if (have_posts()) :
         <li>
             <a style="cursor: pointer" href="<?php the_permalink() ?>">
 
-                <?php $image = get_field('image-bulle');
+                <!--?php $image = get_field('image-bulle');
                 if (has_post_thumbnail()) :
                     $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID))[0];
-                endif; ?>
+                endif; ?-->
 
                 <div class="liste-article"
-                     style="background-image: url(<?php echo $image; ?>);">
-                    <div class="articles">
-                        <div id="fond-texte">
+                     style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>')">
 
-                            <!--Titre et résumé de l'article-->
+                </div>
 
-                            <h2 class="soulignement"><?php the_title(); ?></h2>
-                            <p class="resume"><?php $chaine = substr(get_the_excerpt(), 0, 120);
-                                // on regarde ou se trouve le dernier espace dans la chaine
-                                $espace = strrpos($chaine, " ");
-                                // on prend la chaine de 0 au dernier espace et on ajoute ...
-                                $chaine = substr($chaine, 0, $espace) . " [...]";
-                                echo $chaine; ?></p>
 
-                            <!--Barre en bas du résumé de l'article-->
+                <div id="fond-texte">
 
-                            <div class="en-bas">
-                                <div class="donnees-articles">
-                                    <?php pvc_post_views(get_the_ID(), true); ?> <!--nombre de vues-->
+                    <!--Titre et résumé de l'article-->
 
-                                    <i class="fas fa-comments"></i>
+                    <h2 class="soulignement"><?php the_title(); ?></h2>
+                    <p class="resume"><?php $chaine = substr(get_the_excerpt(), 0, 120);
+                        // on regarde ou se trouve le dernier espace dans la chaine
+                        $espace = strrpos($chaine, " ");
+                        // on prend la chaine de 0 au dernier espace et on ajoute ...
+                        $chaine = substr($chaine, 0, $espace) . " [...]";
+                        echo $chaine; ?></p>
 
-                                    <p class="post-views.entry-meta">
-                                        <?php $num_comments = get_comments_number();
-                                        echo $num_comments; ?> <!--nombre de commentaires-->
-                                    </p>
-                                    <div class="lire_la_suite">
-                                        <p class="interactions">Lire la suite</p>
-                                    </div>
-                                </div>
+                    <!--Barre en bas du résumé de l'article-->
 
-                            </div>
+
+                    <div class="donnees-articles">
+                        <?php pvc_post_views(get_the_ID(), true); ?> <!--nombre de vues-->
+
+                        <i class="fas fa-comments"></i>
+
+                        <p class="post-views.entry-meta">
+                            <?php $num_comments = get_comments_number();
+                            echo $num_comments; ?> <!--nombre de commentaires-->
+                        </p>
+                        <div class="lire_la_suite">
+                            <p class="interactions">Lire la suite</p>
                         </div>
                     </div>
                 </div>
@@ -61,3 +60,20 @@ if (have_posts()) :
     <?php endwhile; ?>
 
 <?php endif; ?>
+
+<!--
+.page-template-listeArticles #event .grille ul {
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+grid-template-columns: repeat(3, 1fr);
+grid-auto-rows: auto;
+grid-column-gap: 4vw;
+grid-row-gap: 0vw;
+margin-left: auto;
+margin-right: auto;
+padding: 2vh;
+list-style-type: none;
+width: 100%;
+}
+-->
