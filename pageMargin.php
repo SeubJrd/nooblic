12573883900823cd9c74a30e66ce6123239af566
Khallan <a.pinot986@laposte.net>
2021-05-26 11:43:45 +0200

    Ajout d'un model de page avec margins

---
+++ pageMargin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Page classique avec margins
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Nooblic
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main" style="margin: 30px">
+
+		<?php the_content(); ?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
